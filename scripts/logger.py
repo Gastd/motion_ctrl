f50c3a6cd8af347bef63c3a5e3c6cd36b2b976c1
Gabriel <gabriel.araujo.5000@gmail.com>
2021-08-04 22:25:36 -0300

    clean code

--- scripts/logger.py
+++ scripts/logger.py
@@ -8,10 +8,6 @@ from geometry_msgs.msg import Twist
 from loggerbib import Logger, LogWriter, ContextualLogger, LogDir, LogLevel
 import roslib.message
 
-linear_sum = 0
-n_linear = 0
-angular_sum = 0
-n_angular = 0
 log_file = None
 log_path = ""
 end_path = ""
@@ -19,24 +15,17 @@ simulation_init_time = 0.0
 simulation_timeout_min = 15
 simulation_timeout_s = simulation_timeout_min*60
 END_KEYWORDS = ["ENDSIM","FAILURE","ENDLOWBATT","ENDTIMEOUTSIM"]
+logwriter = None
+logger = None
 
 class RosTimer():
     def now(self):
         return rospy.get_time() - simulation_init_time
 
 rostimer = RosTimer()
-logwriter = None
-logger = None
-
-def formatlog(loginfo, severity):
-    global simulation_init_time
-    return (str(rospy.get_time() - simulation_init_time) + 
-               ',['+severity+'],'+
-               loginfo)
 
 def check_timeout(event):
     global simulation_timeout_s, simulation_init_time, logger, end_path
-    # end_path = current_path + '/trial.log'
     if (rospy.get_time() - simulation_init_time) > simulation_timeout_s:
         logwriterend = LogWriter(end_path)
         loggerend = Logger(logwriterend, rostimer)
@@ -44,8 +33,6 @@ def check_timeout(event):
 
 def callback_log(data):
     global log_path, simulation_init_time, logger
-    # add time since simulation init into the log
-    # data.data = str(rospy.get_time() - simulation_init_time)+','+data.data
     rospy.loginfo(data.data)
     for keyword in END_KEYWORDS:
         if keyword in data.data:
@@ -63,27 +50,11 @@ def callback_log(data):
             'debug': LogLevel.DEBUG
         }
         rospy.loginfo('content = {}'.format(logdata['content']))
-        # content = unicodedata.normalize('NFKD', '{}'.format(logdata['content'])).encode('ascii', 'ignore')
         logger.log(json.dumps(logdata['content']), entity=logdata['entity'], level=level_map[logdata['level']])
     except Exception as e:
         rospy.logerr(e)
         logger.log(data.data)
 
-    # global linear_sum
-    # global n_linear
-    # global angular_sum
-    # global n_angular
-    # linear_vel = data.linear.x
-    # angular_vel = data.angular.z
-
-    # linear_sum = linear_sum + linear_vel
-    # n_linear = n_linear + 1
-    # angular_sum = angular_sum + angular_vel
-    # n_angular = n_angular + 1
-
-    # rospy.loginfo("ANGULAR: total = %.2f, avg_vel = %.2f, amostras = %d"%(angular_sum, (angular_sum/n_angular), n_angular))
-    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    
 def logger():
     rospy.init_node('logger', anonymous=True)
     global log_path, simulation_init_time, logger, logwriter, end_path
@@ -104,7 +75,6 @@ def logger():
     logger = Logger(logwriter, rostimer)
 
     # open file to log all received data
-    # n_robots = int(os.environ['N_ROBOTS'])
     robot_subs = []
     rospy.loginfo("Initializing simulation logger...")
     # hold node until /clock is initialized
@@ -114,9 +84,6 @@ def logger():
     
     rospy.Timer(rospy.Duration(1), check_timeout)
     simulation_init_time = rospy.get_time() # time in secs
-    # data = String()
-    # data.data = str(rospy.get_time() - simulation_init_time)+',[debug],logger,init,time'
-    # callback_log(data)
 
     logger.log("ROBOTS_CONFIG="+os.environ['ROBOTS_CONFIG'], entity='logger', level=LogLevel.DEBUG)
     rospy.loginfo("ROBOTS_CONFIG="+os.environ['ROBOTS_CONFIG'])
@@ -130,17 +97,11 @@ def logger():
 
     rate = rospy.Rate(1.0/10.0) # 0.1 hz
     while not rospy.is_shutdown():
-        # hello_str = "hello world %s" % rospy.get_time()
-        # rospy.loginfo(hello_str)
-        # pub.publish(hello_str)
         logger.flush()
         rate.sleep()
 
     logger.log('end!')
     logger.flush()
-
-    # spin() simply keeps python from exiting until this node is stopped
-    # rospy.spin()
 
 if __name__ == '__main__':
     try:
